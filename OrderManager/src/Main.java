b5984e982c1d2c79e014d07cd8ace67ae1c1a99b
Andrea <andrea.arnal@ikasle.egibide.org>
2022-12-17 13:13:27 +0100

    Variables comunes current Pieza + Proveedor + Proyecto + Pedido

--- OrderManager/src/Main.java
+++ OrderManager/src/Main.java
@@ -2,21 +2,19 @@ import com.proyecto.PedidosEntity;
 import com.proyecto.PiezasEntity;
 import com.proyecto.ProveedoresEntity;
 import com.proyecto.ProyectosEntity;
-import com.proyecto.controller.PedidosController;
-import com.proyecto.controller.PiezaController;
-import com.proyecto.controller.ProveedorController;
-import com.proyecto.controller.ProyectoController;
-import com.proyecto.utils.HibernateUtil;
 import com.proyecto.views.Home;
 
 import javax.swing.*;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String[] args) {
 
+    //VARIABLES COMPARTIDAS
+    public static PiezasEntity currentPieza = null;
+    public static ProveedoresEntity currentProveedor = null;
+    public static ProyectosEntity currentProyecto = null;
+    public static PedidosEntity currentPedido = null;
+
+    public static void main(String[] args) {
 
         //ABRIR VENTANA!!!
         JFrame frame = new JFrame("Order Manager");
